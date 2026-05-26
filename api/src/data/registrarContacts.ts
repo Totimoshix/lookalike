2e404530bf1d08e08ccf62fd813c55d03ffa1974
lilyful <lilyfulcher@gmail.com>
2026-05-25 21:31:00 -0400

    Update registrarContacts.ts
    Expand registrar contacts for better contact matching.

--- api/src/data/registrarContacts.ts
+++ api/src/data/registrarContacts.ts
@@ -5,7 +5,7 @@ export const registrarContacts: Record<
     abusePortal: string | null;
   }
 > = {
-  "amazon registrar, inc.": {
+    "amazon registrar, inc.": {
     abuseContact: "trustandsafety@support.aws.com",
     abusePortal: "https://support.aws.amazon.com/#/contacts/report-abuse"
   },
@@ -52,5 +52,137 @@ export const registrarContacts: Record<
   "google llc": {
     abuseContact: "registrar-abuse@google.com",
     abusePortal: "https://support.google.com/domains/contact/abuse"
-  }
+  },
+  "web.com group, inc.": {
+    abuseContact: "abuse@web.com",
+    abusePortal: "https://www.web.com/abuse"
+  },
+  "name.com, inc.": {
+    abuseContact: "abuse@name.com",
+    abusePortal: "https://www.name.com/support/articles/abuse"
+  },
+  "enom, llc": {
+    abuseContact: "abuse@enom.com",
+    abusePortal: "https://www.enom.com/resources/abuse.asp"
+  },
+  "register.com, inc.": {
+    abuseContact: "abuse@register.com",
+    abusePortal: "https://www.register.com/legal/aup.rcmx"
+  },
+  "1&1 ionos se": {
+    abuseContact: "abuse@ionos.com",
+    abusePortal: "https://www.ionos.com/terms-gtc/report-abuse/"
+  },
+  "ionos se": {
+    abuseContact: "abuse@ionos.com",
+    abusePortal: "https://www.ionos.com/terms-gtc/report-abuse/"
+  },
+  "pairnic": {
+    abuseContact: "abuse@pair.com",
+    abusePortal: "https://www.pair.com/support/abuse/"
+  },
+  "hover, inc.": {
+    abuseContact: "abuse@hover.com",
+    abusePortal: "https://help.hover.com/hc/en-us/requests/new"
+  },
+  "bluehost inc.": {
+    abuseContact: "abuse@bluehost.com",
+    abusePortal: "https://www.bluehost.com/help/abuse"
+  },
+  "hostgator.com, llc": {
+    abuseContact: "abuse@hostgator.com",
+    abusePortal: "https://www.hostgator.com/abuse"
+  },
+  "squarespace domains i llc": {
+    abuseContact: "abuse-complaints@squarespace.com",
+    abusePortal: "https://support.squarespace.com/hc/en-us/requests/new?ticket_form_id=360000000767"
+  },
+  "wix.com ltd": {
+    abuseContact: "abuse@wix.com",
+    abusePortal: "https://support.wix.com/en/article/reporting-abuse"
+  },
+  "shopify inc.": {
+    abuseContact: "abuse@shopify.com",
+    abusePortal: "https://help.shopify.com/en/manual/your-account/abuse-reporting"
+  },
+  "csc corporate domains, inc.": {
+    abuseContact: "domainabuse@cscglobal.com",
+    abusePortal: "https://www.cscglobal.com/global/web/csc/legal-notices.html"
+  },
+  "verisign, inc.": {
+    abuseContact: "registrar-abuse@verisign.com",
+    abusePortal: "https://www.verisign.com/en_US/domain-names/domain-abuse/index.xhtml"
+  },
+  "public domain registry": {
+    abuseContact: "abuse@publicdomainregistry.com",
+    abusePortal: "https://publicdomainregistry.com/report-abuse.html"
+  },
+  "planet labs, llc": {
+    abuseContact: "abuse@planetlabs.com",
+    abusePortal: null
+  },
+  "ovh sas": {
+    abuseContact: "abuse@ovh.net",
+    abusePortal: "https://www.ovh.com/manager/#/dedicated/hosting/report-abuse"
+  },
+  "online sas": {
+    abuseContact: "abuse@online.net",
+    abusePortal: "https://console.online.net/en/report-abuse"
+  },
+  "aruba s.p.a.": {
+    abuseContact: "abuse@aruba.it",
+    abusePortal: "https://www.aruba.it/en/contacts/abuse.aspx"
+  },
+  "hostinger, uab": {
+    abuseContact: "abuse@hostinger.com",
+    abusePortal: "https://www.hostinger.com/contact"
+  },
+  "internet bs corp.": {
+    abuseContact: "abuse@internetbs.net",
+    abusePortal: "https://www.internetbs.net/abuse.html"
+  },
+  "gransy s.r.o.": {
+    abuseContact: "abuse@gransy.com",
+    abusePortal: null
+  },
+  "cronon ag": {
+    abuseContact: "abuse@cronon.net",
+    abusePortal: null
+  },
+  "hetzner online gmbh": {
+    abuseContact: "abuse@hetzner.com",
+    abusePortal: "https://www.hetzner.com/legal/abuse"
+  },
+  "porkbun llc": {
+    abuseContact: "abuse@porkbun.com",
+    abusePortal: "https://porkbun.com/support"
+  },
+  "namesilo, llc": {
+    abuseContact: "abuse@namesilo.com",
+    abusePortal: "https://www.namesilo.com/contact_us.php"
+  },
+  "epik inc.": {
+    abuseContact: "abuse@epik.com",
+    abusePortal: "https://www.epik.com/index.php?page=ref-abuse"
+  },
+  "spaceship, inc.": {
+    abuseContact: "abuse@spaceship.com",
+    abusePortal: "https://www.spaceship.com/support"
+  },
+  "rebel.com corp.": {
+    abuseContact: "abuse@rebel.com",
+    abusePortal: "https://www.rebel.com/pages/abuse"
+  },
+  "domain.com, llc": {
+    abuseContact: "abuse@domain.com",
+    abusePortal: "https://www.domain.com/info/abuse"
+  },
+  "ca registry (ed rings d/b/a ca domains)": {
+    abuseContact: "abuse@cadomains.com",
+    abusePortal: null
+  },
+  "cira (canadian internet registration authority)": {
+    abuseContact: "registrar@cira.ca",
+    abusePortal: "https://www.cira.ca/en/resources/report-abuse/"
+  },
 };
